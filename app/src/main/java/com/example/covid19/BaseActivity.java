49e12792de2d3461b11c1c3f7e744851360324d0
Devanw <devanewaldo@gmail.com>
2021-11-26 22:55:00 +0700

    activate session manager

--- app/src/main/java/com/example/covid19/BaseActivity.java
+++ app/src/main/java/com/example/covid19/BaseActivity.java
@@ -15,16 +15,16 @@ import java.util.Calendar;
 
 public class BaseActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onResume() {
-//        boolean isAllowed = SessionManagerUtil.getInstance().isSessionActive(this, Calendar.getInstance().getTime());
-//        if (!isAllowed) {
-//            Intent intent = new Intent(this, AuthActivity.class);
-//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//            startActivity(intent);
-//        }
-//        super.onResume();
-//    }
+    @Override
+    protected void onResume() {
+        boolean isAllowed = SessionManagerUtil.getInstance().isSessionActive(this, Calendar.getInstance().getTime());
+        if (!isAllowed) {
+            Intent intent = new Intent(this, AuthActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+        super.onResume();
+    }
 
     protected void showMessage (String msg, DialogInterface.OnClickListener onClick) {
         new AlertDialog.Builder(this)
