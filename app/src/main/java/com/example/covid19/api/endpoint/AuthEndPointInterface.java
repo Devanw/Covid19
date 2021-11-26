2a7447c18c82f8c1aeb9b0eee143a1b4d22555e5
Devanw <devanewaldo@gmail.com>
2021-11-26 22:32:23 +0700

    modify login

--- app/src/main/java/com/example/covid19/api/endpoint/AuthEndPointInterface.java
+++ app/src/main/java/com/example/covid19/api/endpoint/AuthEndPointInterface.java
@@ -1,10 +1,13 @@
 package com.example.covid19.api.endpoint;
 
 import com.example.covid19.model.payload.PayloadLogin;
+import com.example.covid19.model.user.LoginResponse;
 import com.example.covid19.model.user.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 public interface AuthEndPointInterface {
@@ -12,8 +15,10 @@ public interface AuthEndPointInterface {
     // Trailing slash is needed
     String BASE_URL = "https://talentpool.oneindonesia.id/api/";
 
+    @FormUrlEncoded
     @POST("user/login")
     @Headers("X-API-KEY: 454041184B0240FBA3AACD15A1F7A8BB")
-    Call<User> login(@Body PayloadLogin login);
+    Call<LoginResponse> login(@Field("username") String username,
+                              @Field("password") String password);
 
 }
